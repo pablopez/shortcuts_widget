96be6e1cecedb835aa393323f94f1983fcff9307
pablopez <pablolopezjimenez@gmail.com>
2025-06-28 21:19:03 +0200

    Add browser compatibility for Safari and Firefox

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   resolve: {
     alias: { '@': '/src' }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['last 2 Safari versions', 'last 2 Firefox versions'],
+    }),
+  ],
   server: {
     port: 3000,
   },
